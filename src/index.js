f8959d3a4f95b628097c3ec34d84ad44e92c190d
Michael Rios <michael.rios093@gmail.com>
2021-03-01 11:39:44 -0500

    router taken out

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import { render } from 'react-dom';
 import { Provider, connect } from 'react-redux';
 import store, { loadData } from './store';
 import Table from './Table';
-import { HashRouter as Router, Route } from 'react-router-dom';
-
 
 class _App extends Component{
   constructor(){
@@ -21,11 +19,9 @@ class _App extends Component{
   //this works fine now need to figure out how to put my data into Material UI table and add search
   render(){
     return (
-      <Router>
         <div>
-          <Route component={ Table } path = '/' />
+          <Table />
         </div>
-        </Router>
     );
   }
 }
